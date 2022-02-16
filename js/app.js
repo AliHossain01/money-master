d938d2fa2610c3c73b6575b8d299e49cd8c389ee
AliHossain01 <alihossaincse01@gmail.com>
2022-02-17 03:20:00 +0600

    error handale

--- js/app.js
+++ js/app.js
@@ -4,14 +4,26 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     const foodInput = document.getElementById('food-input').value;
     const rentInput = document.getElementById('rent-input').value;
     const othersInput = document.getElementById('others-input').value;
+
     let totalExpenses = document.getElementById('total-expenses');
     let balance = document.getElementById('balance');
 
-    total = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(othersInput);
+    if (typeof incomeInput != 'number') {
+        console.log('Please Enter a Positive Number');
+    }
 
-    totalExpenses.innerText = total;
-    remaining = parseFloat(incomeInput) - total;
-    balance.innerText = remaining;
+    else if (incomeInput >= 0 && foodInput >= 0 && rentInput >= 0 && othersInput >= 0) {
+        total = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(othersInput);
+
+        totalExpenses.innerText = total;
+        remaining = parseFloat(incomeInput) - total;
+        balance.innerText = remaining;
+    }
+    else {
+        console.log('Please Enter a Positive Number');
+    }
+
+
 
 });
 
@@ -19,9 +31,12 @@ document.getElementById('save-money').addEventListener('click', function () {
     const incomeInput = document.getElementById('income-input');
     let incomeInputValue = incomeInput.value;
     let balance = document.getElementById('balance').innerText;
-    let remainingBalanceNumber = document.getElementById('remaining-balance');
-    const saveMoneyInput = document.getElementById('save-money-input').value;
+
     const savingAmount = document.getElementById('saving-amount');
+    let remainingBalanceNumber = document.getElementById('remaining-balance');
+
+    const saveMoneyInput = document.getElementById('save-money-input').value;
+
     let savesMoney = (incomeInputValue * saveMoneyInput) / 100;
     remainingBalance = parseFloat(balance) - savesMoney;
     savingAmount.innerText = savesMoney;
